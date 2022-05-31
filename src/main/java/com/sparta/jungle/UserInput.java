da2a9ec929e36f390fd660aa3b2253e60695e54c
AH <andyhop@hotmail.co.uk>
2022-05-31 17:30:47 +0100

    Comitting Tests, Input and Output classes

--- src/main/java/com/sparta/jungle/UserInput.java
+++ src/main/java/com/sparta/jungle/UserInput.java
@@ -1,7 +1,15 @@
 package com.sparta.jungle;
 
+import java.util.Scanner;
+
 public class UserInput {
 
-    public UserInput() {
+
+
+    public static int getMonths() {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter number of months to simulate: ");
+        int months = Integer.parseInt(userInput.nextLine());
+        return months;
     }
 }
--- src/test/java/com/sparta/jungle/RandomGeneratorTest.java
+++ src/test/java/com/sparta/jungle/RandomGeneratorTest.java
@@ -1,7 +1,13 @@
 package com.sparta.jungle;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 class RandomGeneratorTest {
+    @Test
+    @DisplayName("Assert true")
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
 
 }
