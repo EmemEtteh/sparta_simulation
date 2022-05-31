1946ddca65b42489626733bc9ae6322733b8e0a9
acharge91 <ashtoncharge@gmail.com>
2022-05-31 15:25:51 +0100

    created trainee manager class logic

--- src/main/java/com/sparta/jungle/TraineeManager.java
+++ src/main/java/com/sparta/jungle/TraineeManager.java
@@ -2,6 +2,24 @@ package com.sparta.jungle;
 
 public class TraineeManager {
 
-    public TraineeManager() {
+    static int totalTrainees = 0;
+    static int allocatedTrainees = 0;
+
+    public static void main(String[] args) {
+        getTotalTrainees(5);
+    }
+
+    public static void getTotalTrainees(int numberOfMonths) {
+        for (int i = 0; i <= numberOfMonths; i++) {
+            totalTrainees += getRandomTrainees();
+        }
+    }
+
+    public static int getAllocatedTrainees() {
+        return allocatedTrainees;
+    }
+
+    public static void setAllocatedTrainees(int calculatedAllocatedTrainees) {
+        allocatedTrainees = calculatedAllocatedTrainees;
     }
 }
