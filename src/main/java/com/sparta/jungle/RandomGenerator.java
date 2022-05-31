fef0548e6ebf47758a825b66b7ed506bb9e63bea
Emem Etteh <eetteh@spartaglobal.com>
2022-05-31 16:43:47 +0100

    made all methods static

--- src/main/java/com/sparta/jungle/RandomGenerator.java
+++ src/main/java/com/sparta/jungle/RandomGenerator.java
@@ -1,12 +1,35 @@
 package com.sparta.jungle;
+import java.util.Random;
+
+
 
 public class RandomGenerator {
 
-    public int getRandomTrainees(){
 
+
+    public static int getRandomTrainees(){
+
+        Random random = new Random();
+        int minTrainees = 50;
+        int maxTrainees = 100;
+
+        int randomNumberOfNewTrainees = random.nextInt(maxTrainees - minTrainees) + minTrainees;
+        return randomNumberOfNewTrainees;
     }
 
-    public int getRandomCentreIntake(){
+    public static int getRandomCentreIntake(){
+        Random random = new Random();
+        int minTrainees = 0;
+        int maxTrainees = 50;
 
+        int randomNumberOfIntookTrainees = random.nextInt(maxTrainees - minTrainees) + minTrainees;
+        return randomNumberOfIntookTrainees;
     }
+
+//    public static void main(String[] args) {
+//
+//        System.out.println(getRandomTrainees());
+//    }
 }
+
+
