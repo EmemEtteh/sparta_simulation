fb52b0d0f24e527cf667cf75f85779cefdebb88f
Josh A <joshaustin312@gmail.com>
2022-05-31 13:34:25 +0100

    added RandomGeneratorTest class

--- src/main/java/com/sparta/jungle/RandomGenerator.java
+++ src/main/java/com/sparta/jungle/RandomGenerator.java
@@ -7,6 +7,6 @@ public class RandomGenerator {
     }
 
     public int getRandomCentreIntake(){
-        
+
     }
 }
---
+++ src/test/java/com/sparta/jungle/RandomGeneratorTest.java
@@ -0,0 +1,7 @@
+package com.sparta.jungle;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RandomGeneratorTest {
+
+}
